d25787158039d4c2ee9ac48619d3fc082aa473ac
Eduardo <eduardokunmixyeah@gmail.com>
2024-08-16 20:55:05 -0400

    patients table, search bar done

--- src/pages/patients/Patients.tsx
+++ src/pages/patients/Patients.tsx
@@ -1,64 +1,83 @@
-import { IPatients, patients,  } from './Patients.data'
+import { IPatients, patients, } from './Patients.data'
 import './Patiens.css'
 import '../pages.css'
 
-export const Patients = () =>{
+export const Patients = () => {
 
 
-    return(
+    return (
         <div className='pages-index'>
-        <div className='pages-index'>
-            <div className='start-pages'>
-                <h2>PACIENTES</h2>
-                <div className='user-info'>
-                    <div className="fullname">
-                        <h2>Nombre y Apellido</h2>
-                    </div>
-                    <div className="user-type">
-                        <h2>(Tipo de Usuario)</h2>
+            <div className='pages-index'>
+                <div className='start-pages'>
+                    <h2>PACIENTES</h2>
+                    <div className='user-info'>
+                        <div className="fullname">
+                            <h2>Nombre y Apellido</h2>
+                        </div>
+                        <div className="user-type">
+                            <h2>(Tipo de Usuario)</h2>
+                        </div>
                     </div>
                 </div>
+                <hr />
+                <div className="add_patients">
+                    <div className='search_field'>
+                        <input type="text" placeholder='Buscar Paciente.......' />
+                    </div>
+                    <button>
+                        <i className="fa-solid fa-circle-plus" /> AÑADIR PACIENTE
+                    </button>
+                </div>
+                <table className='content-table'>
+                    <thead>
+                        <tr>
+                            <th>NOMBRE Y APELLIDO</th>
+                            <th>CÉDULA DE IDENTIDAD</th>
+                            <th>NÚMERO DE TELEFONO</th>
+                            <th>CÓDIGO DE HISTORIA</th>
+                            <th>OPCIONES</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {patients.map((patient: IPatients) => (
+                            <tr>
+                                <td>{patient.full_name}</td>
+                                <td>{patient.id_number}</td>
+                                <td>{patient.phone_number}</td>
+                                <td>{patient.mhistory_id}</td>
+                                <td>
+                                    <div className="buttons">
+                                        
+                                        <div className='add_button'>
+                                            <a>
+                                                <i className={patient.add_button} />
+                                            </a>
+                                        </div>
+
+                                        <div className='info_button'>
+                                            <a>
+                                                <i className={patient.info_button} />
+                                            </a>
+                                        </div>
+
+                                        <div className='edit_button'>
+                                            <a>
+                                                <i className={patient.edit_button} />
+                                            </a>
+                                        </div>
+
+                                        <div className='delete_button'>
+                                            <a>
+                                                <i className={patient.delete_button} />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-            <hr />
-            <div className="add_patients">
-            <button><i className="fa-solid fa-circle-plus"/> AÑADIR PACIENTE</button>
-            </div>
-            <table className='content-table'>
-                <thead>
-                    <tr>
-                        <th>NOMBRE Y APELLIDO</th>
-                        <th>CÉDULA DE IDENTIDAD</th>
-                        <th>NÚMERO DE TELEFONO</th>
-                        <th>CÓDIGO DE IDENTIFICACIÓN</th>
-                        <th>OPCIONES</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {patients.map((patient: IPatients) => (
-                    <tr>
-                        <td>{patient.full_name}</td>
-                        <td>{patient.id_number}</td>
-                        <td>{patient.phone_number}</td>
-                        <td>{patient.mhistory_id}</td>
-                        <td>
-                            <a className='add_button'>
-                                <i className={patient.add_button}/>
-                            </a>
-                            <a className='info_button'>
-                                <i className={patient.info_button}/>
-                            </a>
-                            <a className='edit_button'>
-                                <i className={patient.edit_button}/>
-                            </a>
-                            <a className='delete_button'>
-                                <i className={patient.delete_button}/>
-                            </a>
-                        </td>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
         </div>
-    </div>
     )
 }
